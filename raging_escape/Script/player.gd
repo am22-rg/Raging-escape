db4c0d984d4f7c36f81210b1aa3a9e4b6bd8a116
am22-rg <amartin@students.mags.school.nz>
2026-06-19 09:49:52 +1200

    Day #1
    Character movement added tilemaplayer added started to add the healthbar but i need to program them into the player so they will change when the player takes damage I have added the camera so that it follows the player and started with the ui for the menu thats not fully finished. tommorw I try to finish the health and corruption bars and the ui for the menu

---
+++ raging_escape/Script/player.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+var health: int = 12 
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+@export var health_bar_ui: ProgressBar
+@export var Corrution_bar_ui: ProgressBar
+
+func Take_damage():
+	if health > 1:
+		health -= 1
+		health_bar_ui.value = health
+	else:
+		pass
+		# TODO Menu - make a you died menu
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("Up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("Left", "Right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()

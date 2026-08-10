f1c56887d035f70bcf21eacc9ff9f52b4c76e565
am22-rg <amartin@students.mags.school.nz>
2026-08-10 14:43:04 +1200

    Day #17
    making the screen shake

--- raging_escape/Script/main.gd
+++ raging_escape/Script/main.gd
@@ -1,13 +1,31 @@
 extends Node2D
 
 signal reset
+
+var trauma := 0.0
+var multi_power: int = 2
+var corruption_val
+
 @export var ui: Control
+
+@export var decay: float = 0.8
+@export var max_offset: Vector2 = Vector2(30, 20)
+@onready var camera: Camera2D = $Camera2D
+
+
+func shake():
+	trauma * corruption_val
+	var amount = pow(trauma, multi_power)
+	camera.offset.x = max_offset * amount
 
 
 func _ready() -> void:
 	get_tree().paused = true
 	ui.connect("pause_game", pause_game)
 	ui.connect("unpause_game", unpause_game)
+	
+	# Get a connection to the signal manager for screen shake 
+	SignalManager.corruption_sig.connect(update_corruption)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,6 +34,10 @@ func _process(_delta: float) -> void:
 		pause_game()
 		ui.level_select()
 		ui.show()
+
+
+func update_corruption():
+	pass
 
 
 func pause_game() -> void:
--- raging_escape/Script/player.gd
+++ raging_escape/Script/player.gd
@@ -40,8 +40,6 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("Up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("Left", "Right")
 	if direction:
 		velocity.x = direction * SPEED
